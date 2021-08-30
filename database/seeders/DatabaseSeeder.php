f41dfde5bb562e32bdb99c6c35f1d14c7d7b478b
theone89 <franklincampos8907@gmail.com>
2021-08-30 13:35:19 -0400

    Factory & Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        Storage::deleteDirectory('posts');
+        Storage::makeDirectory('posts');
+
+      $this->call(UserSeeder::class);
+      Category::factory(4)->create();
+      Tag::factory(8)->create();
+      $this->call(PostSeeder::class);
+
     }
 }
