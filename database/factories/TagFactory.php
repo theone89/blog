fae334df7971ad09c88c07b8ed445c7e91771254
theone89 <franklincampos8907@gmail.com>
2021-08-31 17:13:14 -0400

    problemas en la css

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -28,6 +28,7 @@ class TagFactory extends Factory
         return [
             'name' => $name,
             'slug' => Str::slug($name),
+            'color' => $this->faker->randomElement(['red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'])
         ];
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,27 @@
 <x-app-layout>
 
-    <div class="container bg-red-500">
-        dsafksj
+    <div class="container py-8">
+        <div class="grid grid-cols-3 gap-6">
+            @foreach ($posts as $post)
+                <article  class="w-full h-80 bg-cover bg-center @if ($loop->first) col-span-2 @endif" style="background-image: url({{Storage::url($post->image->url)}})">
+                    <div class="w-full h-full px-8 flex flex-col justify-center">
+                        <div>
+                            @foreach ($post->tags as $tag)
+                            <a href="http://" class="inline-block px-3 h-6 bg-{{$tag->color}}-600 text-white rounded-full">{{$tag->name}}</a>
+                            @endforeach
+
+                        </div>
+                        <h1 class="text-4xl text-white leading-8 font-bold">
+                            <a href="">
+                                {{$post->name}}
+
+                            </a>
+                        </h1>
+                    </div>
+                </article>
+            @endforeach
+
+        </div>
     </div>
 
 </x-app-layout>
