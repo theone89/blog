4eea3f5b8832d0f323f1149cadb600cfa265fcf6
theone89 <franklincampos8907@gmail.com>
2021-09-01 15:21:43 -0400

    todo ok filtros etiquetas y categorias ok

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Tag;
 
 class PostController extends Controller
 {
@@ -35,5 +36,12 @@ class PostController extends Controller
                         ->paginate(6);
 
         return view('posts.category', compact('posts', 'category'));
+    }
+
+    public function tag(Tag $tag){
+
+        $posts = $tag->posts()->where('status', 2)->latest('id')->paginate(4);
+
+        return view('posts.tag', compact('posts', 'tag'));
     }
 }
--- resources/views/posts/tag.blade.php
+++ resources/views/posts/tag.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 class="uppercase text-center text-3xl font-bold">Categoria: {{ $category->name }}
+        <h1 class="uppercase text-center text-3xl font-bold">Etiqueta: {{ $tag->name }}
         </h1>
 
         @foreach ($posts as $post)
