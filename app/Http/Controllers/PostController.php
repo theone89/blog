18e14a28c5e838c4d3fa80e30bc79bfc8f679537
theone89 <franklincampos8907@gmail.com>
2021-09-01 12:45:01 -0400

    Todo ok vid 7 completo

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,22 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     //
-    public function index(){
+    public function index()
+    {
         $posts = Post::where('status', 2)->paginate(8);
 
         return view('posts.index', compact('posts'));
+    }
+    public function show(Post $post)
+    {
+
+        $similares = Post::where('category_id', $post->category_id)
+                            ->where('status', 2)
+                            ->where('id', '!=', $post->id)
+                            ->latest('id')
+                            ->take(4)
+                            ->get();
+
+        return view('posts.show', compact('post', 'similares'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
+Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
