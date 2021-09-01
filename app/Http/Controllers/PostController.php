32437664d1aaae02be4ea597ecf4e446a3d735c4
theone89 <franklincampos8907@gmail.com>
2021-09-01 13:52:22 -0400

    Todo ok Vid 8

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class PostController extends Controller
 {
@@ -25,5 +26,14 @@ class PostController extends Controller
                             ->get();
 
         return view('posts.show', compact('post', 'similares'));
+    }
+
+    public function category(Category $category){
+        $posts = Post::where('category_id', $category->id)
+                        ->where('status', 2)
+                        ->latest('id')
+                        ->paginate(6);
+
+        return view('posts.category', compact('posts', 'category'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
+Route::get('category/{category}', [PostController::class, 'category'])->name('posts.category');
+
+
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
